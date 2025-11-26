76e37302c6988f651c0ed0e53baa8ea070d17134
Matteo <matteoadamo02@gmail.com>
2025-11-26 11:46:53 +0100

    Update SmarttrackingApplication.java

--- src/main/java/it/univr/smarttracking/SmarttrackingApplication.java
+++ src/main/java/it/univr/smarttracking/SmarttrackingApplication.java
@@ -1,13 +1,12 @@
-package it.univr.smarttracking;
+package com.smarttracking.valid_soft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmarttrackingApplication {
+public class SmartTrackingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmarttrackingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTrackingApplication.class, args);
+    }
 }
